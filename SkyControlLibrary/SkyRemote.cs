ecb3dbb85dc2b0abfd77e5bb48f0f6733d01a9b8
Brett Hewitt <brett_hewitt@live.co.uk>
2020-02-02 18:41:57 +0000

    Code refactor

--- SkyControlLibrary/SkyRemote.cs
+++ SkyControlLibrary/SkyRemote.cs
@@ -39,32 +39,31 @@ namespace SkyControlLibrary
             var commandBytes = new byte[] { 4, 1, 0, 0, 0, 0, (byte)Math.Floor(224 + ((int)code / 16d)), (byte)((int)code % 16)};
             var commandBytes2 = new byte[] { 4, 0, 0, 0, 0, 0, (byte)Math.Floor(224 + ((int)code / 16d)), (byte)((int)code % 16) };
 
-            TcpClient client = new TcpClient(Host, Port);
-            NetworkStream ns = client.GetStream();
-            
-            ns.WriteTimeout = 1000;
-            ns.ReadTimeout = 1000;
-            try
+            using (TcpClient client = new TcpClient(Host, Port))
+            using (NetworkStream ns = client.GetStream())
             {
-                byte[] handShake = new byte[24];
-                ns.Read(handShake, 0, 12);
-                ns.Write(handShake, 0, 12);
-                ns.Read(handShake, 0, 2);
-                ns.Write(handShake, 0, 1);
-                ns.Read(handShake, 0, 4);
-                ns.Write(handShake, 0, 1);
-                ns.Read(handShake, 0, 24);
+                ns.WriteTimeout = ConnectTimeout;
+                ns.ReadTimeout = ConnectTimeout;
 
-                ns.Write(commandBytes, 0, commandBytes.Length);
-                ns.Write(commandBytes2, 0, commandBytes2.Length);
+                try
+                {
+                    byte[] handShake = new byte[24];
+                    ns.Read(handShake, 0, 12);
+                    ns.Write(handShake, 0, 12);
+                    ns.Read(handShake, 0, 2);
+                    ns.Write(handShake, 0, 1);
+                    ns.Read(handShake, 0, 4);
+                    ns.Write(handShake, 0, 1);
+                    ns.Read(handShake, 0, 24);
+
+                    ns.Write(commandBytes, 0, commandBytes.Length);
+                    ns.Write(commandBytes2, 0, commandBytes2.Length);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
             }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
-            
-            ns.Dispose();
-            client.Close();
         }
 
         public void Press(SkyCommands button)
